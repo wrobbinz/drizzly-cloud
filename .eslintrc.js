d932601ffca7b64ff1878d4ece944b95f94412a8
wrobbinz <wrobbinz@gmail.com>
2017-10-28 18:53:25 -0700

    add babel plugin for react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
+    parser: 'babel-eslint',
     extends: 'airbnb',
     rules: {
         semi: [2, 'never']
     },
     globals: {
         document: true,
+        window: true,
     },
 }
