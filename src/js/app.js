7c5c0ce6dbfb9aa46b727a782fe90dca3ada39da
wrobbinz <wrobbinz@gmail.com>
2017-11-01 22:57:18 -0700

    main run logic goes in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,19 @@
+import getWords from './requests'
+import wordCloud from './cloud'
 import '../css/style.css'
+
+
+const INTERVAL = 4000
+
+async function createCloud(cloud) {
+  let words
+  try {
+    words = await getWords()
+  } catch (err) {
+    console.log(err.message)
+  }
+  cloud.update(words)
+  setTimeout(() => { createCloud(cloud) }, INTERVAL)
+}
+// Start cycle
+createCloud(wordCloud('#cloud'))
